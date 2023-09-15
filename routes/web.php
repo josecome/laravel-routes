e8462b5663b9fbf9b5b91af089c81ac0056b2468
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-15 21:26:24 +0200

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* Simple Route with callback function */
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Dinamic Route*/
+Route::get('/page/{slug}', function ($slug) {});
+
+/* Dinamic Route that only accept letters as parameter */
+Route::get('/page/{slug}', function ($slug) {})->where('slug', '[A-Za-z]+');
+
+/* Dinamic Route that only accept number as parameter */
+Route::get('/page/{id}', function ($id) {})->where('id', '[0-9]+');
+
+/* Option 1: Dinamic Route that only accept number as first parameter and letters as second parameter */
+Route::get('/page/{id}/{slug}', function ($id, $name) {})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+/* Option 2: Dinamic Route that only accept number as first parameter and letters as second parameter */
+Route::get('/page/{id}/{name}', function ($id, $name) {})->whereNumber('id')->whereAlpha('name');
+
+Route::get('/page/{name}', function ($name) {})->whereAlphaNumeric('name');
+
+Route::get('/page/{id}', function ($id) {})->whereUuid('id');
+
+Route::get(
+    '/page/{category}', function ($category) {}
+)->whereIn('category', ['movie', 'song', 'painting']);
+
+Route::get('page/{slug}', function ($slug) {})->can('update', $post);
+
+//Grouped route
+Route::middleware('auth')->group(function () {
+    Route::get('page/{slug}', function ($slug) {});
+    Route::get('category/{slug}', function ($slug) {});
+});
+
+//Optional Parameters
+Route::get('/page/{slug?}', function (string $slug = null) {
+    return $slug;
+});
+
